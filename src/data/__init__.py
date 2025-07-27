167f25606a6daaecdec49f2f8de02c762a0be03b
sreevarshan-xenoz <sreevarshan1511@gmail.com>
2025-07-27 08:37:54 +0530

    Implement data ingestion and loading capabilities

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,1 +1,13 @@
-# Data ingestion and preprocessing components
+"""
+Data ingestion and preprocessing components for network intrusion detection.
+"""
+
+from .interfaces import DatasetLoader, PreprocessingPipeline, FeatureProcessor
+from .loaders import BaseNetworkDatasetLoader
+
+__all__ = [
+    'DatasetLoader',
+    'PreprocessingPipeline', 
+    'FeatureProcessor',
+    'BaseNetworkDatasetLoader',
+]
